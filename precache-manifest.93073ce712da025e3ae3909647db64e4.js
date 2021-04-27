e2362867f2f75951995e3f6b05921f304d2f6715
annisokaay <momynov.sultan>
2021-04-27 23:27:43 +0600

    Updates

---
+++ precache-manifest.93073ce712da025e3ae3909647db64e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e4af7b27d86c7028f528ff341212ed",
+    "url": "/react-smartremont-task/index.html"
+  },
+  {
+    "revision": "787d2d610692039efd97",
+    "url": "/react-smartremont-task/static/css/main.e2427b6c.chunk.css"
+  },
+  {
+    "revision": "2c3a3d8adeea30cf239a",
+    "url": "/react-smartremont-task/static/js/2.33a358b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-smartremont-task/static/js/2.33a358b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "787d2d610692039efd97",
+    "url": "/react-smartremont-task/static/js/main.8fc98163.chunk.js"
+  },
+  {
+    "revision": "06378ffb73b32d8c0abc",
+    "url": "/react-smartremont-task/static/js/runtime-main.0b6ebc1a.js"
+  }
+]);
